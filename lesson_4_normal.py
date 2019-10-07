45beb667e2e2c3c5c0292aee2338392086fcef89
alexkorm14 <alex.kormishenkov@gmail.com>
2019-10-07 13:06:53 +0300

    выполнил уровень easy and normal задания уровня hard стараюсь решать в свободное время не совсем разобрался с реугялрными выражениями: понял, что означают символы, но как pattern оформлять пока не могу

---
+++ lesson_4_normal.py
@@ -0,0 +1,172 @@
+# Задание-1:
+# Вывести символы в нижнем регистре, которые находятся вокруг
+# 1 или более символов в верхнем регистре.
+# Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
+# Решить задачу двумя способами: с помощью re и без.
+import re
+line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
+       'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
+       'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
+       'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa'\
+       'pzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWete'\
+       'kUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQ'\
+       'WrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXb'\
+       'JrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCC'\
+       'EUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfB'\
+       'tQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuT'\
+       'SkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCu'\
+       'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB'\
+       'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
+       'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
+       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGccccccc'
+
+#без re
+
+lst = []
+line_new = ''
+i=0
+while i <len(line):
+    if line[i].islower():
+        line_new  = line_new + str(line[i])
+    else:
+        lst.append(line_new)
+        line_new = ''
+        j=i
+    i+=1
+while j<len(line):
+    if line[j].islower():
+        line_new  = line_new + str(line[j])
+    else:
+        lst.append(line_new)
+        line_new = ''
+    j+=1
+lst_final = []
+for el in lst:
+    if el!='':
+        lst_final.append(el)
+
+print(lst_final)
+
+
+# Задание-2:
+# Вывести символы в верхнем регистре, слева от которых находятся
+# два символа в нижнем регистре, а справа - два символа в верхнем регистре.
+# Т.е. из строки 
+# "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
+# нужно получить список строк: ['AY', 'NOGI', 'P']
+# Решить задачу двумя способами: с помощью re и без.
+
+line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
+       'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
+       'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA'\
+       'HqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIV'\
+       'jXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnW'\
+       'etekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfC'\
+       'vzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbR'\
+       'uXbJrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkm'\
+       'jCCEUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOn'\
+       'LfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGS'\
+       'euTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCf'\
+       'KCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWH'\
+       'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
+       'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
+       'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+
+# без re
+lst = []
+line_new = ''
+i = -1
+j=0
+
+while i<(len(line_2)-5):
+    i=i+1
+    if line_2[i].islower() and line_2[i+1].islower() and line_2[i+2].isupper() :
+        j=i+2
+        while line_2[j+1].isupper() and line_2[j+2].isupper():
+            line_new = line_new + str(line_2[j])
+            j=j+1
+        lst.append(line_new)
+        line_new= ""
+lst_final = []
+for el in lst:
+    if el!='':
+        lst_final.append(el)
+
+print(lst_final)
+
+# Задание-3:
+# Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
+# произвольными целыми цифрами, в результате в файле должно быть
+# 2500-значное произвольное число.
+# Найдите и выведите самую длинную последовательность одинаковых цифр
+# в вышезаполненном файле.
+import random
+import os
+
+file = open('/Users/aleksandrkormisenkov//desktop/big_number.txt', 'w')
+i=0
+number =''
+while i<2500:
+  number = number + str(random.randint(0,9))
+  i+=1
+file.write(str(number))
+file.close()
+
+file = open('/Users/aleksandrkormisenkov//desktop/big_number.txt')
+big_number=file.read()
+j=0
+lst = []
+repeat_number = ''
+while j<len(big_number)-1:
+    if big_number[j]==big_number[j+1]:
+        repeat_number = repeat_number + str(big_number[j])+ str(big_number[j+1])
+    else:
+      lst.append(repeat_number)
+      repeat_number = ''
+    j+=1
+lst_final = []
+for el in lst:
+    if el!='':
+        lst_final.append(el)
+n=0
+maxx = 0
+for repeat_number in range(len(lst_final)):
+  if len(lst_final[n]) > maxx:
+        maxx = len(lst_final[n])
+        k=n
+        maxx_repeat = maxx
+  n+=1
+file.close()
+file = open('/Users/aleksandrkormisenkov//desktop/big_number.txt', 'a')
+file.write('Наибольшая последовательность\n')
+file.write(str(lst_final[k]))
+file.close()
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
